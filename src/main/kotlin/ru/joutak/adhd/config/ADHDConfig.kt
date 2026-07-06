2a87dce800c0c6f040ff28bcf184d3f1e0c53335
Ga_Mes <boyIgnat@gmail.com>
2026-07-06 03:23:27 +0300

    Добавлены карты в конфиг.

--- src/main/kotlin/ru/joutak/adhd/config/ADHDConfig.kt
+++ src/main/kotlin/ru/joutak/adhd/config/ADHDConfig.kt
@@ -2,11 +2,15 @@ package ru.joutak.adhd.config
 
 import org.bukkit.configuration.file.YamlConfiguration
 import ru.joutak.adhd.ADHDPlugin
+import ru.joutak.adhd.world.SpawnPoint
 import java.io.File
 
 object ADHDConfig {
 
     var maxPlayers: Int = 4
+        private set
+
+    var maps = mapOf<Int, Map<Int, SpawnPoint>>()
         private set
 
     fun load() {
@@ -19,5 +23,38 @@ object ADHDConfig {
         val config = YamlConfiguration.loadConfiguration(file)
 
         maxPlayers = config.getInt("default.maxPlayers", 4)
+
+        maps = loadMaps(config)
+
+        ADHDPlugin.instance.logger.info("Карты: $maps")
+    }
+
+    fun loadMaps(config: YamlConfiguration): Map<Int, Map<Int, SpawnPoint>> {
+        val result = mutableMapOf<Int, MutableMap<Int, SpawnPoint>>()
+
+        val maps = config.getConfigurationSection("maps") ?: return emptyMap()
+
+        for (mapId in maps.getKeys(false)) {
+            val mapIndex = mapId.toInt()
+            val spawns = maps.getConfigurationSection("$mapId.spawns") ?: continue
+
+            val spawnMap = mutableMapOf<Int, SpawnPoint>()
+
+            for (spawnId in spawns.getKeys(false)) {
+                val spawnIndex = spawnId.toInt()
+
+                spawnMap[spawnIndex] = SpawnPoint(
+                    x = spawns.getDouble("$spawnId.x"),
+                    y = spawns.getDouble("$spawnId.y"),
+                    z = spawns.getDouble("$spawnId.z"),
+                    yaw = spawns.getDouble("$spawnId.yaw").toFloat(),
+                    pitch = spawns.getDouble("$spawnId.pitch").toFloat()
+                )
+            }
+
+            result[mapIndex] = spawnMap
+        }
+
+        return result
     }
 }
