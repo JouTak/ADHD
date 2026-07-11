503c6a4083e50a11a743d314707375be016e781a
Ga_Mes <boyIgnat@gmail.com>
2026-07-11 18:45:11 +0300

    Начало исполнения турнира по статусам.

--- src/main/kotlin/ru/joutak/adhd/tournament/Tournament.kt
+++ src/main/kotlin/ru/joutak/adhd/tournament/Tournament.kt
@@ -1,12 +1,42 @@
 package ru.joutak.adhd.tournament
 
+import org.bukkit.scheduler.BukkitRunnable
 import ru.joutak.adhd.ADHDPlugin
 import ru.joutak.adhd.world.Arena
 import java.util.*
 
 class Tournament(val participants: MutableList<UUID>, val modesPool: List<String>) {
 
+    var tick = 0L
+
+    var currentMode = ""
+
+    var modePointer = 0
+
+    val ticker = object : BukkitRunnable() {
+        override fun run() {
+            tick()
+        }
+    }
+
+    var status = TournamentStatus.START
+
+    lateinit var worldName: String
+
+    lateinit var adjustedMaps: Map<Int, List<Arena>>
+
     fun start(worldName: String, adjustedMaps: Map<Int, List<Arena>>) {
-        ADHDPlugin.instance.logger.info("Start ($this): $worldName $modesPool $participants $adjustedMaps")
+        ADHDPlugin.instance.logger.info("Начата игра в мире $worldName, кол-во игроков ${participants.size}, размер пула режимов ${modesPool.size}")
+
+        this.worldName = worldName
+        this.adjustedMaps = adjustedMaps
+
+        status = TournamentStatus.PREPARING
+
+        ticker.runTaskTimer(ADHDPlugin.instance, 0L, 2L)
+    }
+
+    fun tick() {
+
     }
 }
---
+++ src/main/kotlin/ru/joutak/adhd/tournament/TournamentStatus.kt
@@ -0,0 +1,5 @@
+package ru.joutak.adhd.tournament
+
+enum class TournamentStatus {
+    START, PREPARING, RUNNING, FINISH
+}
