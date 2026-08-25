30fac9a3fe8d832a0b47e94cecbe1df237a691ad
Ignat <boyIgnat@gmail.com>
2026-08-26 02:12:24 +1000

    Тепорт на спавн.

--- src/main/kotlin/ru/joutak/adhd/game/concrete/MemoryGame.kt
+++ src/main/kotlin/ru/joutak/adhd/game/concrete/MemoryGame.kt
@@ -1,5 +1,9 @@
 package ru.joutak.adhd.game.concrete
 
+import org.bukkit.Bukkit
+import org.bukkit.GameMode
+import org.bukkit.Location
+import org.bukkit.entity.Player
 import ru.joutak.adhd.game.Game
 import ru.joutak.adhd.game.GameState
 import ru.joutak.adhd.game.mode.meta.ModeMeta
@@ -27,6 +31,25 @@ class MemoryGame : Game() {
         this.worldName = worldName
         this.arena = arena
         this.members = members
+
+        for (uuid in members) {
+            val player = Bukkit.getPlayer(uuid) ?: continue
+
+            setPlayer(player)
+        }
+    }
+
+    fun setPlayer(player: Player) {
+        player.inventory.clear()
+
+        player.gameMode = GameMode.ADVENTURE
+        player.health = 20.0
+        player.saturation = 20.0f
+        player.foodLevel = 20
+
+        val spawn = arena.spawnPoints.random()
+
+        player.teleport(Location(Bukkit.getWorld(worldName)!!, spawn.x, spawn.y, spawn.z, spawn.yaw, spawn.pitch))
     }
 
     override fun update() {
