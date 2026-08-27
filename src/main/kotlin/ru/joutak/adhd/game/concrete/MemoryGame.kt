9c5bd6c0433be9769d9d6ebb1f77844e58bd4822
Ignat <boyIgnat@gmail.com>
2026-08-27 22:44:09 +1000

    Разные цвета.

--- src/main/kotlin/ru/joutak/adhd/game/concrete/MemoryGame.kt
+++ src/main/kotlin/ru/joutak/adhd/game/concrete/MemoryGame.kt
@@ -1,10 +1,14 @@
 package ru.joutak.adhd.game.concrete
 
 import org.bukkit.Bukkit
+import org.bukkit.Color
 import org.bukkit.GameMode
 import org.bukkit.Location
+import org.bukkit.Material
 import org.bukkit.entity.ArmorStand
 import org.bukkit.entity.Player
+import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.meta.LeatherArmorMeta
 import ru.joutak.adhd.config.map.meta.concrete.MemoryMapMeta
 import ru.joutak.adhd.game.Game
 import ru.joutak.adhd.game.GameState
@@ -13,6 +17,7 @@ import ru.joutak.adhd.world.Arena
 import ru.joutak.adhd.world.SpawnPoint
 import java.util.UUID
 import kotlin.math.floor
+import kotlin.random.Random
 
 class MemoryGame : Game() {
 
@@ -29,6 +34,8 @@ class MemoryGame : Game() {
     lateinit var points: List<SpawnPoint>
 
     val stands = mutableSetOf<ArmorStand>()
+
+    val colorStands = mutableMapOf<ArmorStand, Color>()
 
     override fun start(
         worldName: String,
@@ -72,7 +79,35 @@ class MemoryGame : Game() {
             stand.isInvulnerable = true
             stand.setArms(false)
             stand.setBasePlate(true)
+
+            equipStand(stand)
         }
+    }
+
+    fun colorItem(material: Material, color: Color): ItemStack {
+        val item = ItemStack(material, 1)
+
+        val meta = item.itemMeta as LeatherArmorMeta
+
+        meta.setColor(color)
+
+        item.itemMeta = meta
+
+        return item
+    }
+
+    fun equipStand(stand: ArmorStand) {
+        val color = Color.fromRGB(Random.nextInt(256), Random.nextInt(256), Random.nextInt(256))
+
+        stand.equipment.setHelmet(colorItem(Material.LEATHER_HELMET, color))
+
+        stand.equipment.setChestplate(colorItem(Material.LEATHER_CHESTPLATE, color))
+
+        stand.equipment.setLeggings(colorItem(Material.LEATHER_LEGGINGS, color))
+
+        stand.equipment.setBoots(colorItem(Material.LEATHER_BOOTS, color))
+
+        colorStands[stand] = color
     }
 
     fun setPlayer(player: Player) {
