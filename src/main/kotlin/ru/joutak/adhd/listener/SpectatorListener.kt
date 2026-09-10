ad8ceccb74dae8943108cd36301e8b6ab3047d09
Ga_Mes <boyIgnat@gmail.com>
2026-09-10 21:58:32 +0300

    Фикс.

--- src/main/kotlin/ru/joutak/adhd/listener/SpectatorListener.kt
+++ src/main/kotlin/ru/joutak/adhd/listener/SpectatorListener.kt
@@ -4,6 +4,8 @@ import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.EntityDamageByEntityEvent
+import org.bukkit.event.player.PlayerInteractEntityEvent
+import org.bukkit.event.player.PlayerInteractEvent
 import ru.joutak.adhd.tournament.TournamentManager
 
 class SpectatorListener : Listener {
@@ -15,6 +17,24 @@ class SpectatorListener : Listener {
         val tournament = TournamentManager.playerTournaments[damager.uniqueId] ?: return
 
         if (damager.uniqueId in tournament.spectators) {
+            event.isCancelled = true
+        }
+    }
+
+    @EventHandler
+    fun onInteract(event: PlayerInteractEvent) {
+        val tournament = TournamentManager.playerTournaments[event.player.uniqueId] ?: return
+
+        if (event.player.uniqueId in tournament.spectators) {
+            event.isCancelled = true
+        }
+    }
+
+    @EventHandler
+    fun onInteractEntity(event: PlayerInteractEntityEvent) {
+        val tournament = TournamentManager.playerTournaments[event.player.uniqueId] ?: return
+
+        if (event.player.uniqueId in tournament.spectators) {
             event.isCancelled = true
         }
     }
