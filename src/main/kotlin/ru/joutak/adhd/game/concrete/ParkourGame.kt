59452887c0809876b3365330d6db2db6a0c63d25
Ignat <boyIgnat@gmail.com>
2026-09-14 15:11:23 +0300

    Телепорт игроков. Фикс бага спавна финишей.

--- src/main/kotlin/ru/joutak/adhd/game/concrete/ParkourGame.kt
+++ src/main/kotlin/ru/joutak/adhd/game/concrete/ParkourGame.kt
@@ -1,14 +1,18 @@
 package ru.joutak.adhd.game.concrete
 
 import org.bukkit.Bukkit
+import org.bukkit.GameMode
 import org.bukkit.Location
 import org.bukkit.entity.Interaction
+import org.bukkit.entity.Player
 import ru.joutak.adhd.config.map.meta.concrete.ParkourMapMeta
 import ru.joutak.adhd.game.Game
 import ru.joutak.adhd.game.GameState
 import ru.joutak.adhd.game.mode.meta.ModeMeta
 import ru.joutak.adhd.world.Arena
+import ru.joutak.adhd.world.SpawnPoint
 import java.util.UUID
+import kotlin.math.floor
 
 class ParkourGame : Game() {
 
@@ -24,6 +28,8 @@ class ParkourGame : Game() {
 
     val finishes = mutableSetOf<Interaction>()
 
+    var lSpawn: SpawnPoint? = null
+
     override fun start(
         worldName: String,
         arena: Arena,
@@ -38,8 +44,12 @@ class ParkourGame : Game() {
 
         val world = Bukkit.getWorld(worldName)!!
 
+        val xOffset = floor(arena.spawnPoints[0].x / 512) * 512
+
+        val zOffset = floor(arena.spawnPoints[0].z / 512) * 512
+
         for (p in meta.finish) {
-            val loc = Location(world, p.x, p.y, p.z)
+            val loc = Location(world, p.x + xOffset, p.y, p.z + zOffset)
 
             val interaction = world.spawn(loc, Interaction::class.java) {
                 it.interactionWidth = 1f
@@ -48,6 +58,42 @@ class ParkourGame : Game() {
 
             finishes.add(interaction)
         }
+
+        for (uuid in members) {
+            val player = Bukkit.getPlayer(uuid) ?: continue
+
+            teleportToSpawn(player)
+
+            restoreStats(player)
+        }
+    }
+
+    fun teleportToSpawn(player: Player) {
+        val world = Bukkit.getWorld(worldName)!!
+
+        val spawns = arena.spawnPoints.toMutableSet()
+
+        if (lSpawn != null) {
+            spawns -= mutableSetOf(lSpawn!!)
+        }
+
+        val chosen: SpawnPoint = if (spawns.isEmpty()) {
+            lSpawn!!
+        } else {
+            spawns.random()
+        }
+
+        lSpawn = chosen
+
+        player.teleport(Location(world, chosen.x, chosen.y, chosen.z, chosen.yaw, chosen.pitch))
+    }
+
+    fun restoreStats(player: Player) {
+        player.gameMode = GameMode.ADVENTURE
+        player.health = 20.0
+        player.saturation = 20.0f
+        player.foodLevel = 20
+        player.inventory.clear()
     }
 
     override fun update() {
