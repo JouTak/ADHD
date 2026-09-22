80da2992e254a6964418883e8c1274917c295d3c
SlavaGabrielyan <Slavachessplayer@mail.ru>
2026-09-22 21:04:42 +0300

    Режим добавлен, механика рикошета работает

--- src/main/kotlin/ru/joutak/adhd/game/mode/meta/concrete/RicochetArenaModeMeta.kt
+++ src/main/kotlin/ru/joutak/adhd/game/mode/meta/concrete/RicochetArenaModeMeta.kt
@@ -2,12 +2,4 @@ package ru.joutak.adhd.game.mode.meta.concrete
 
 import ru.joutak.adhd.game.mode.meta.ModeMeta
 
-class RicochetArenaModeMeta(    val pointsToWin: Int,
-                                val projectileSpeed: Double,
-                                val maxBounces: Int,
-                                val lifetimeTicks: Long,
-                                val cooldownTicks: Int,
-                                val projectileDamage: Double) : ModeMeta() {
-
-
-}
+class RicochetArenaModeMeta(val pointsToWin: Int, val projectileSpeed: Double, val maxBounces: Int, val lifetimeTicks: Long, val cooldownTicks: Int, val projectileDamage: Double) : ModeMeta() {}
