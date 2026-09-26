78789e222c38ef22f24eab8aa5afd6976a28737b
fedotow-p <kirisemga@gmail.com>
2026-09-26 17:02:04 +0300

    removed line with chatListener

--- src/main/kotlin/ru/joutak/adhd/ADHDPlugin.kt
+++ src/main/kotlin/ru/joutak/adhd/ADHDPlugin.kt
@@ -40,7 +40,6 @@ class ADHDPlugin : JavaPlugin() {
         Bukkit.getPluginManager().registerEvents(ru.joutak.adhd.listener.mode.snipers.RespawnListener(), instance)
         Bukkit.getPluginManager().registerEvents(ru.joutak.adhd.listener.mode.knights.DismountListener(), instance)
         Bukkit.getPluginManager().registerEvents(ru.joutak.adhd.listener.mode.knights.FireListener(), instance)
-        Bukkit.getPluginManager().registerEvents(ru.joutak.adhd.listener.mode.rps.ChatListener(), instance)
         Bukkit.getPluginManager().registerEvents(ru.joutak.adhd.listener.mode.casino.GuiListener(), instance)
         Bukkit.getPluginManager().registerEvents(ru.joutak.adhd.listener.mode.casino.InteractListener(), instance)
         Bukkit.getPluginManager().registerEvents(ru.joutak.adhd.listener.mode.memory.HitListener(), instance)
